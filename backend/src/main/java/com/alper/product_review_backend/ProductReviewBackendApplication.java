d9285defebc73c8f14cf0faa93b0c5ba6489c5e9
Alper <h.alper4283@gmail.com>
2026-01-06 02:51:09 +0300

    Initialize Spring Boot backend

---
+++ backend/src/main/java/com/alper/product_review_backend/ProductReviewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alper.product_review_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductReviewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductReviewBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alper/product_review_backend/ProductReviewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alper.product_review_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductReviewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
